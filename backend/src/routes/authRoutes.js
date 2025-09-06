409c942b3a55c624a0d825f3a67d86d53e606960
FelipeHM5 <10felipehidal15@gmail.com>
2025-09-06 02:07:22 -0500

    Se agregaron las rutas /user, /update/:id y /delete/:id para los endpoints

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { register, login} = require('../controllers/authController');
+const { register, login, getProfile, editProfile, deleteProfile} = require('../controllers/authController');
 
 // Rutas de autenticación
 router.post('/register', register);
 router.post('/login', login);
+router.get('/users', getProfile);
+router.put('/update/:id', editProfile);
+router.delete('/delete/:id', deleteProfile);
 
 
 
